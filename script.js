f814d3ae0d0a747c9fbfcdd332fffb92fabf51cd
Moira Leon <moira.leon2@gmail.com>
2022-03-12 18:07:08 -0700

    correcting callback function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const option1 = true
 const option2 = false
 
-function watchTutorialCallback(callback, errorCallback){
+function sampleCallback(callback, errorCallback){
     if(option1){
         errorCallback({
             name: 'option1',
@@ -17,9 +17,25 @@ function watchTutorialCallback(callback, errorCallback){
     }
 }
 
-watchTutorialCallback((message) =>{
+sampleCallback((message) =>{
     console.log('Success: ' + message)
 },(error) =>{
     console.log(error.name + ' ' + error.message)
 }
 )
+
+function samplePromise(callback, errorCallback){
+    if(option1){
+        errorCallback({
+            name: 'option1',
+            message: 'Chocolate is best'
+        })
+    }else if (option2){
+        errorCallback({
+            name: 'option2',
+            message: 'Strawberry is best'
+        })
+    }else{
+        callback('Vanilla is best')
+    }
+}
